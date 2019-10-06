a1bc5da6ab3eee2155f5b761caafeac95b60245b
Shilton <shilton.sistemas@gmail.com>
2019-10-06 18:11:52 -0300

    Implementing Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/logo.svg';
+import Routes from './routes'
 
 function App() {
   return (
    <div className="container">
       <img src={logo} alt="logo" className="logo"/>
-
       <div className="content">
-        <p>
-          Ofereça <strong> spots </strong> para programadores e contre <strong> talentos</strong>
-        </p>
-
-        <form>
-          <label htmlFor="email"> E-mail *</label>
-          <input  type="email" 
-                  id="email" 
-                  placeholder="Seu e-mail"
-          />
-
-          <button type="button" className="btn">Entrar</button>
-
-        </form>
-
+          <Routes/>
       </div>
    </div>
   );
